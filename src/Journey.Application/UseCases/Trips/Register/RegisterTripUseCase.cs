a8515a57b6d2dd18bd1ba43f794302e2553311e3
antonioRenato <antoniorpfs@gmail.com>
2025-02-05 05:50:08 -0300

    Created Excpetion and Endpoint post

---
+++ src/Journey.Application/UseCases/Trips/Register/RegisterTripUseCase.cs
@@ -0,0 +1,25 @@
+﻿using Journey.Communication.Requests;
+using Journey.Exception.ExceptionsBase;
+
+namespace Journey.Application.UseCases.Trips.Register
+{
+    public class RegisterTripUseCase
+    {
+        public void Execute(RequestRegisterTripJson request)
+        {
+            Validate(request);
+        }
+
+        private void Validate(RequestRegisterTripJson request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Name))
+                throw new JourneyException("Nome não pode ser vazio");
+
+            if (request.StartDate.Date < DateTime.UtcNow.Date)
+                throw new JourneyException("Data de começo não pode estar no passado!");
+
+            if (request.EndDate.Date >= DateTime.UtcNow.Date)
+                throw new JourneyException("A viagem deve terminar após a data de inicio");
+        }
+    }
+}
