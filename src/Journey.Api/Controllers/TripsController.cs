bd93024aefc6e240d77b5785436eb2ba5979deb4
antonioRenato <antoniorpfs@gmail.com>
2025-02-16 20:09:58 -0300

    created endpoint GetById and GetAll

--- src/Journey.Api/Controllers/TripsController.cs
+++ src/Journey.Api/Controllers/TripsController.cs
@@ -1,5 +1,8 @@
-﻿using Journey.Application.UseCases.Trips.Register;
+﻿using Journey.Application.UseCases.Trips.GetAll;
+using Journey.Application.UseCases.Trips.GetById;
+using Journey.Application.UseCases.Trips.Register;
 using Journey.Communication.Requests;
+using Journey.Communication.Responses;
 using Journey.Exception.ExceptionsBase;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,6 +13,8 @@ namespace Journey.Api.Controllers
     public class TripsController : ControllerBase
     {
         [HttpPost]
+        [ProducesResponseType(typeof(ResponseShortTripJson), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
         public IActionResult Register([FromBody] RequestRegisterTripJson request)
         {
             try
@@ -30,6 +35,39 @@ namespace Journey.Api.Controllers
             }
         }
 
+        [HttpGet]
+        [ProducesResponseType(typeof(ResponseTripsJson), StatusCodes.Status200OK)]
+        public IActionResult GetAll()
+        {
+            var useCase = new GetAllTripsUseCase();
 
+            var result = useCase.Execute();
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        [ProducesResponseType(typeof(ResponseTripJson), StatusCodes.Status200OK)]            
+        [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]            
+        public IActionResult GetById([FromRoute] Guid id) 
+        {
+            try
+            {
+                var useCase = new GetByIdUseCase();
+
+                var response = useCase.Execute(id);
+
+                return Ok(response);
+            }
+            catch (JourneyException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error desconhecido");
+            }
+        }
     }
 }
